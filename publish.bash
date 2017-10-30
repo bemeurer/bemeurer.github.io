87468d93aa6ed31f5be07a36cd51f74da72c7c3d
Bernardo Meurer <meurerbernardo@gmail.com>
2017-10-30 11:37:52 -0700

    Better publishing

--- publish.bash
+++ publish.bash
@@ -1,9 +1,10 @@
 #! /usr/bin/env bash
 
 git checkout source
-bundle exec jekyll build
+JEKYLL_ENV=production bundle exec jekyll build
 git checkout master 
 rsync -av ./_site/* ./
 git add *
-git commit && git push
+git commit || true
+git push || true
 git checkout source
