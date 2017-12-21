587c181077382c5c6b6b2d824dad83b9ba5dfa46
Bernardo Meurer <meurerbernardo@gmail.com>
2017-12-20 18:32:59 -0800

    Revert deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,10 +26,10 @@ if [ -n "$TRAVIS_BUILD_ID" ]; then
   #   GIT_NAME         - The Git user name
   #   GIT_EMAIL        - The Git user email
   #
-  echo DEPLOY_BRANCH: "$DEPLOY_BRANCH"
-  echo ENCRYPTION_LABEL: "$ENCRYPTION_LABEL"
-  echo GIT_NAME: "$GIT_NAME"
-  echo GIT_EMAIL: "$GIT_EMAIL"
+  echo DEPLOY_BRANCH: $DEPLOY_BRANCH
+  echo ENCRYPTION_LABEL: $ENCRYPTION_LABEL
+  echo GIT_NAME: $GIT_NAME
+  echo GIT_EMAIL: $GIT_EMAIL
   if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
     echo "Travis should only deploy from the DEPLOY_BRANCH ($DEPLOY_BRANCH) branch"
     exit 0
@@ -43,24 +43,24 @@ if [ -n "$TRAVIS_BUILD_ID" ]; then
       REPO=${REPO/git:\/\/github.com\//git@github.com:}
       REPO=${REPO/https:\/\/github.com\//git@github.com:}
 
-      chmod 600 "$SSH_KEY"
-      eval "$(ssh-agent -s)"
-      ssh-add "$SSH_KEY"
+      chmod 600 $SSH_KEY
+      eval `ssh-agent -s`
+      ssh-add $SSH_KEY
       git config --global user.name "$GIT_NAME"
       git config --global user.email "$GIT_EMAIL"
     fi
   fi
 fi
 
-REPO_NAME=$(basename "$REPO")
-TARGET_DIR=$(mktemp -d /tmp/"$REPO_NAME.XXXX")
+REPO_NAME=$(basename $REPO)
+TARGET_DIR=$(mktemp -d /tmp/$REPO_NAME.XXXX)
 REV=$(git rev-parse HEAD)
-git clone --branch ${TARGET_BRANCH} "${REPO}" "${TARGET_DIR}"
-rsync -rt --delete --exclude=".git" "$SOURCE_DIR/" "$TARGET_DIR/"
-cd $"TARGET_DIR"
+git clone --branch ${TARGET_BRANCH} ${REPO} ${TARGET_DIR}
+rsync -rt --delete --exclude=".git" $SOURCE_DIR/ $TARGET_DIR/
+cd $TARGET_DIR
 git add -A .
 git commit --allow-empty -m "Built from commit $REV"
-git push "$REPO" "$TARGET_BRANCH"
+git push $REPO $TARGET_BRANCH
 
 git config --global user.name "$GIT_NAME"
 git config --global user.email "$GIT_EMAIL"
